87aca3b60a8affaeefcd991d4aa9af7c3a22db91
StringEpsilon <StringEpsilon@gmail.com>
2019-04-06 17:59:01 +0200

    CI: Publish test details and coverage HTML report

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
 	modulePaths: ["<rootDir>/node_modules"],
 	setupFiles: ["raf/polyfill"],
 	testMatch: ["**/src/**/__tests__/**/*test.(t|j)s?(x)"],
-	testURL: "http://localhost/"
+	testURL: "http://localhost/",
+	testResultsProcessor: "jest-nunit-reporter",
 };
 
