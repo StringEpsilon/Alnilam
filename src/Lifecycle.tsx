d5b2fd8362d4e04bc025b7ab00c5a80c0497ed38
StringEpsilon <StringEpsilon@gmail.com>
2019-04-10 20:47:07 +0200

    Chore: Simplify Lifecycle.tsx

--- src/Lifecycle.tsx
+++ src/Lifecycle.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
 export interface LifecycleProps {
-	onMount?: (component: Lifecycle) => void;
-	onUpdate?: (component: Lifecycle, prevProps: any) => void;
+	onMount: (component: Lifecycle) => void;
+	onUpdate: (component: Lifecycle, prevProps: any) => void;
 	onUnmount?: (component: Lifecycle) => void;
 	to?: any; // for Redirect.tsx
 	message?: any; // for Prompt.tsx
@@ -12,18 +12,18 @@ export default class Lifecycle extends React.Component<LifecycleProps> {
 	public release: ((message?: string) => void) | undefined;
 
 	public componentDidMount(): void {
-		if (this.props.onMount) { this.props.onMount.call(this, this); }
+		this.props.onMount.call(this, this);
 	}
 
 	public componentDidUpdate(prevProps: any): void {
-		if (this.props.onUpdate) { this.props.onUpdate.call(this, this, prevProps); }
+		this.props.onUpdate.call(this, this, prevProps);
 	}
 
 	public componentWillUnmount(): void {
 		if (this.props.onUnmount) { this.props.onUnmount.call(this, this); }
 	}
 
-	public render(): JSX.Element | null {
+	public render(): null {
 		return null;
 	}
 }
