1e21a89a9fe5661c36bf5e6e65df8275e81083d3
StringEpsilon <StringEpsilon@gmail.com>
2019-04-16 23:08:15 +0200

    Chore: Dependency housekeeping.

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,8 @@ export { default as Redirect } from "./Redirect";
 export { default as Link } from "./Link";
 export { default as NavLink } from "./NavLink";
 export { default as StaticRouter } from "./StaticRouter";
-
 export { default as withRouter } from "./withRouter";
 export { default as generatePath } from "./generatePath";
 export { default as matchPath } from "./matchPath";
-
 export { RouterContext } from "./RouterContext";
 export { useRouter } from "./useRouter";
