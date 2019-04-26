907e4dc6b4d696197a55baf180b377f018be1e13
StringEpsilon <StringEpsilon@gmail.com>
2019-04-26 21:19:59 +0200

    Fix: Only show navlink stub warning in development
    By explicetely comparing against 'development', unit tests won't be spammed with the warning.

--- src/NavLink-stub.ts
+++ src/NavLink-stub.ts
@@ -1,5 +1,5 @@
 
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV === "development") {
 	// tslint:disable-next-line:no-console
 	console.warn("NavLink has been merged with Link. The export will be dropped in the future.");
 }
